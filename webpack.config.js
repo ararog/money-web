494b6b867f5c0591911c03edd36cf8adcc565eb1
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-06-11 09:04:46 -0300

    Removing old dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,33 @@ var webpack = require('webpack');
 var bourbon = require('node-bourbon').includePaths;
 
 module.exports = {
-    entry: [
-        'webpack-hot-middleware/client',
-        './js/app.js'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
-    },
-    resolve: {
-        extensions: ['', '.webpack.js', '.web.js', '.js']
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-				/*
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        })
-				*/
-    ],
-    module: {
-        loaders: [
-            {exclude: /localforage/, test: /\.js$/, loader: 'babel' },
-            {test: require.resolve('chart.js'), loader: 'imports?this=>window'},
-            {test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + bourbon }
-        ]
-    }
+  entry: [
+    'webpack-hot-middleware/client',
+    './js/app.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.js']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+		/*
+    new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+            warnings: false
+        }
+    })
+		*/
+  ],
+  module: {
+    loaders: [
+      {exclude: /localforage/, test: /\.js$/, loader: 'babel' },
+      {test: require.resolve('chart.js'), loader: 'imports?this=>window'},
+      {test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + bourbon }
+    ]
+  }
 }
